934c2819bba9d2434624a0923ebe02654c555beb
mariano.palliser <mariano.palliser@gmail.com>
2024-02-10 15:15:23 +0100

    implement filter output and manage observable

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core'
+import {
+  Component, EventEmitter, OnDestroy, OnInit, Output,
+} from '@angular/core'
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { MatInputModule } from '@angular/material/input'
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
+import {
+  Subject, debounceTime, filter, takeUntil,
+} from 'rxjs'
 
 @Component({
   selector: 'app-filter',
@@ -10,6 +15,24 @@ import { MatButtonModule } from '@angular/material/button'
   imports: [MatInputModule, FormsModule, ReactiveFormsModule, MatIconModule, MatButtonModule],
   templateUrl: 'filter.component.html',
 })
-export class FilterComponent {
-  formControl = new FormControl('')
+export class FilterComponent implements OnInit, OnDestroy {
+  public formControl = new FormControl<string>('', { nonNullable: true })
+
+  @Output() onFilter = new EventEmitter<string>()
+
+  private destroy = new Subject<void>()
+
+  ngOnInit(): void {
+    this.formControl.valueChanges
+      .pipe(
+        takeUntil(this.destroy),
+        filter((value: string) => !!value),
+        debounceTime(300),
+      ).subscribe((value: string) => this.onFilter.emit(value))
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next()
+    this.destroy.complete()
+  }
 }
