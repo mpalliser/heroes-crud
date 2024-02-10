e086f578f0bbfa2867c0ec74abbaaff9f68a6c89
mariano.palliser <mariano.palliser@gmail.com>
2024-02-10 15:37:39 +0100

    implement basic list

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core'
+import { MatIconModule } from '@angular/material/icon'
+import { MatTableModule } from '@angular/material/table'
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatIconModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.sass',
 })
 export class ListComponent {
+  public displayedColumns: string[] = ['id', 'name', 'actions']
 
+  public data: unknown[] = [
+    { id: 1, name: 'test' },
+    { id: 2, name: 'superman' },
+    { id: 3, name: 'spiderman' },
+    { id: 4, name: 'batman' },
+    { id: 5, name: 'lobezno' },
+  ]
 }
