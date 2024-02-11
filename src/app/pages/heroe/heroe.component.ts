238f66918f9109c15d8eb79c446a99c38d6e472a
mariano.palliser <mariano.palliser@gmail.com>
2024-02-11 15:48:06 +0100

    add navigation between components

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit, inject } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
+import { Component, inject } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { ActivatedRoute, Router } from '@angular/router'
 import { FormComponent } from '../../components/form/form.component'
 
 @Component({
   selector: 'app-heroe',
   standalone: true,
-  imports: [FormComponent],
+  imports: [FormComponent, MatIconModule, MatButtonModule],
   templateUrl: './heroe.component.html',
   styleUrl: './heroe.component.sass',
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent {
   public title: string | undefined = inject(ActivatedRoute).snapshot.title
 
+  private router = inject(Router)
   // private readonly route = inject(ActivatedRoute).snapshot.paramMap.get('id')
 
-  ngOnInit(): void {
+  public goToHeroes(): void {
+    this.router.navigate([''])
   }
 }
--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, inject } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { Router } from '@angular/router'
 import { FilterComponent } from '../../components/filter/filter.component'
 import { ListComponent } from '../../components/list/list.component'
 import { Heroe } from '../../models/heroe'
@@ -7,21 +9,32 @@ import { HeroeService } from '../../services/heroe.service'
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FilterComponent, ListComponent],
+  imports: [FilterComponent, ListComponent, MatButtonModule],
   template: `
-    <app-filter (onFilterChanges)="filterHeroes($event)"></app-filter>
-    <app-list [heroes]="heroes"></app-list>
+    <div class="container">
+      <div class="is-flex is-justify-content-space-between is-align-items-center">
+        <app-filter (onFilterChanges)="filterHeroes($event)"></app-filter>
+        <button mat-raised-button color="primary" (click)="goToNewHero()">Nuevo heroe</button>
+      </div>
+      <app-list [heroes]="heroes"></app-list>
+    </div>
   `,
 })
 export class HeroesComponent {
   public heroes: Heroe[] = []
 
-  private heroeService = inject(HeroeService)
+  private readonly heroeService = inject(HeroeService)
 
-  filterHeroes(filter: string): void {
+  private readonly router = inject(Router)
+
+  public filterHeroes(filter: string): void {
     this.heroes = []
     this.heroeService.filterHeroes(filter).subscribe((heroes: Heroe[]) => {
       this.heroes = heroes
     })
+  }
+
+  public goToNewHero(): void {
+    this.router.navigate(['new'])
   }
 }
